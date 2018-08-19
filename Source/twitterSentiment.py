a3f34c59d0cf3dea7a71cf0fa6e62fb24c576fd8
xCloudx8 <danielestrife@gmail.com>
2018-08-19 12:48:03 +0200

    Added arrays for deeper analysis

--- Source/twitterSentiment.py
+++ Source/twitterSentiment.py
@@ -2,14 +2,18 @@
 import tweepy
 #import TextBlob from textblob
 from textblob import TextBlob
+# Each word in the lexicon has scores for:
+# 1)     polarity: negative vs. positive    (-1.0 => +1.0)
+# 2)     subjectivity: objective vs. subjective (+0.0 => +1.0)
+# 3)    intensity: modifies next word?      (x0.5 => x2.0)
 
 #make four variables which stores your keys as in string
 #you can get these from your twitter api registered dashboard
-consumer_key='<your consumer key>'
-consumer_secret='<your consumer secret>'
+consumer_key='TAnYsf1oMVSqJcqmSgTUaaxFm'
+consumer_secret='1OIMTW57hTaZffektXBwA7PFvwsFu2WsS6KssPiFHHrbNP12lU'
 
-access_token='<your access token>'
-access_token_secret='<your access token secret>'
+access_token='61872867-xFsISv0Q1fM2HfK8O0kKf8CLjJX093upSD9MyWqz1'
+access_token_secret='oKlbTKBSP6j8wE0TtMSXm7EdfJk3CX2kkdeOwO6xTTjjh'
 #create a varable which helps as to authicate with twitter api
 auth=tweepy.OAuthHandler(consumer_key,consumer_secret)
 #set access foryour access_token
@@ -18,13 +22,26 @@ auth.set_access_token(access_token,access_token_secret)
 api=tweepy.API(auth)
 #make a variable which stores all the tweets which contains the word you are searching for
 #here i search all the tweets which contain the word cricket
-public_tweets=api.search('cricket')
+public_tweets=api.search('Renzi')
+
+# Put sentiments in arrays
+polarity = []
+subjectivity = []
 
 #make a for loop and access all the tweets one by one
 for tweet in public_tweets:
     #print the tweets if you want
-    print(tweet.text)
+    #print(tweet.text)
     #make a variable which stores a list which contains the split words using TextBlob.
     analysis=TextBlob(tweet.text)
+    
     #print sentiment of each using sentiment
-    print(analysis.sentiment)
+    
+    #print(analysis.sentiment)
+    #print(analysis.polarity)
+    polarity.append(analysis.polarity)
+    #print(analysis.subjectivity)
+    subjectivity.append(analysis.subjectivity)
+
+print sum(polarity)/len(polarity)
+print sum(subjectivity)/len(subjectivity)
